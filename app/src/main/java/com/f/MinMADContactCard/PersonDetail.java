251b939d08dbe270e57828d8e20b1da666cbd785
fervandisseldorp <fervandisseldorp@gmail.com>
2017-10-03 17:09:35 +0200

    first commit

---
+++ app/src/main/java/com/f/MinMADContactCard/PersonDetail.java
@@ -0,0 +1,18 @@
+package com.f.MinMADContactCard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ferdinand on 3-10-2017.
+ */
+
+public class PersonDetail extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
